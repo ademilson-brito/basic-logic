63ef36029d2be38417b6c2dae5cfc1da7a328ade
mano <ademilsonbrito07@gmail.com>
2023-04-07 16:04:29 -0300

    trabalhei multiplicação com if

--- testes/taboada.js
+++ testes/taboada.js
@@ -6,3 +6,12 @@ var resultado = numero1 * numero2;
 if (resultado > 10) {
     console.log("numero1 e maior que 10");
 }
+
+var tomate = 2;
+var cebola = 8;
+
+var compras = tomate * cebola;
+
+if (compras > 10) {
+    console.log("as compras do mes foi maior que 10");
+}
